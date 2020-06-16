9444d26c1499424ee92becc88215a7abc47f56f3
kiku ishimoto <kikuish@gmail.com>
2020-06-16 00:41:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ class App < Sinatra::Base
   get "/goodbye/:name" do
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
-  end 
-  get "/multiply/:num1/:num2" do 
+  end
+  get "/multiply/:num1/:num2" do
     @number1 = params[:num1]
     @number2 = params[:num2]
 
